9a649051815c487e5e8b4103bef08a750f367884
Hana <hanah9512@gmail.com>
2021-01-27 19:55:54 +0900

    add_params

--- 3.result/videocap.py
+++ 3.result/videocap.py
@@ -2,7 +2,7 @@ import cv2
 import os
 import time
 
-def video_cap(videoFile, imgpath, time_skips):
+def video_cap(videoFile, imgpath,intro_skips=0.0,time_skips=1.0):
     start = time.time()
     cap = cv2.VideoCapture(videoFile)
     count = 0
@@ -11,7 +11,7 @@ def video_cap(videoFile, imgpath, time_skips):
         if ret :
             cv2.imwrite(imgpath+"/frame%d.jpg" % count, frame)
             print('%d.jpg done' % count)
-            cap.set(cv2.CAP_PROP_POS_MSEC,(count*time_skips))
+            cap.set(cv2.CAP_PROP_POS_MSEC,intro_skips + count*time_skips)
             count += 1
         
         else:
@@ -21,3 +21,5 @@ def video_cap(videoFile, imgpath, time_skips):
 
     runtime = 'runtime : {}'.format(time.time()-start)
     return runtime
+
+#.py 만든 커널
